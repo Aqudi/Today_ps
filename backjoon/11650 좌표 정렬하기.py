e03978764ea75b065d0d51b73a7e7eeadebd1f5e
Aqudi <gjdigj145@gmail.com>
2021-09-07 14:53:22 +0900

    backjoon 11650 좌표 정렬

---
+++ backjoon/11650 좌표 정렬하기.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline().rstrip())
+ps = []
+for i in range(N):
+    ps.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+ps = sorted(ps, key=lambda x: x[1])
+ps = sorted(ps, key=lambda x: x[0])
+
+for p in ps:
+    print(p[0], p[1]) 
