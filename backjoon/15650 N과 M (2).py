e3d97d44d570d55676fb2540da063df61332832f
Aqudi <gjdigj145@gmail.com>
2021-09-10 14:18:24 +0900

    backjoon 15650 N과 M (2)

---
+++ backjoon/15650 N과 M (2).py
@@ -0,0 +1,21 @@
+def pop(x):
+    return x.pop() if len(x) else None
+
+def solve(ns, step, N, M):
+    for i in range(ns[-1], N+1):
+        if step <= 1:
+            if  len(ns) == M:
+                print(' '.join(map(str, ns)))
+                pop(ns)
+                ns.append(i+1)
+        else:
+            ns.append(i+1)
+            if step > 0:
+                solve(ns, step-1, N, M)
+            pop(ns)
+        pop(ns)
+        ns.append(i+1)
+
+
+N, M = map(int, input().split())
+solve([1], M, N, M)
